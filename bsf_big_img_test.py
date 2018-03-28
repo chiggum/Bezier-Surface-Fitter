9f9e9714cd5976f46feaad957cec45aa5abca7c2
chiggum <dhruvkohli99@gmail.com>
2018-03-28 18:36:14 +0530

    separate model for each image, works with fitter and plotter

--- bsf_big_img_test.py
+++ bsf_big_img_test.py
@@ -18,9 +18,9 @@ np.random.seed(27)
 rn.seed(27)
 tf.set_random_seed(27)
 
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+# session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
 
-sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+sess = tf.Session()
 K.set_session(sess)
 ################################################################################################
 ################################################################################################
@@ -34,7 +34,7 @@ import cv2
 
 fname = "data/tree.png"
 img = cv2.imread(fname)
-# img = cv2.resize(img, (int(img.shape[0]/4),int(img.shape[1]/4)))
+img = cv2.resize(img, (int(img.shape[0]/4),int(img.shape[1]/4)))
 
 img_rows = img.shape[0]
 img_cols = img.shape[1]
@@ -48,36 +48,45 @@ img /= col_scale_div
 
 n_img = 1
 img_batch = img
-m = 20
-n = 20
+m = 40
+n = 40
 
-model = bs_fit(img_batch, m, n)
-K_mat_raw = bs_get_weights(model)
-K_mat = np.reshape(K_mat_raw, (n_img,img_channels,m+1,n+1))
-dummy_input = np.zeros((1, 1)).astype(np.float32)
-pred = model.predict(dummy_input)
+bez_input = bez_filter(img_rows, img_cols, m, n)
+models = []
+for i in range(n_img):
+    models.append(bs_fit(img_batch[i,:], bez_input, m, n))
+
+
+preds = []
+for i in range(n_img):
+    preds.append(np.reshape(models[i].predict(bez_input), (img_rows,img_cols,img_channels)))
+
 
 # for analysis
+bez_input = None    # free up memory
 H = 2*img_rows
 W = 2*img_cols
-fine_model = bs_build_model(n_img,img_channels,H,W,m,n)
-bs_set_weights(fine_model, K_mat_raw)
-fine_pred = fine_model.predict(dummy_input)
-mydir = "analysis/chunk_analysis_"+str(m)+"_"+str(n)
+fine_bez_input = bez_filter(H, W, m, n)
+fine_preds = []
+for i in range(n_img):
+    fine_preds.append(np.reshape(models[i].predict(fine_bez_input), (H,W,img_channels)))
+
+mydir = "analysis/big_img_sep_models_analysis_"+str(m)+"_"+str(n)
 if not os.path.exists(mydir):
     os.makedirs(mydir)
 
 
 for i in range(n_img):
-    bsplot(m, n, H, W, np.transpose(fine_pred[0,i,:],[1,2,0]), mydir+"/bsf_full_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
+    bsplot(m, n, H, W, fine_preds[i], mydir+"/bsf_full_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
     fig=plt.figure(figsize=(8, 8))
     columns = 3
     rows = 1
     fig.add_subplot(rows, columns, 1)
     plt.imshow(np.transpose(img_batch[i,:], [1,2,0]), cmap="gray")
     fig.add_subplot(rows, columns, 2)
-    plt.imshow(np.transpose(np.clip(pred[0, i,:],0,1), [1,2,0]), cmap="gray")
+    plt.imshow(np.clip(preds[i],0,1), cmap="gray")
     fig.add_subplot(rows, columns, 3)
-    plt.imshow(np.transpose(np.abs(img_batch[i,:]-np.clip(pred[0, i,:],0,1)), [1,2,0]), cmap="gray")
+    plt.imshow(np.abs(np.transpose(img_batch[i,:], [1,2,0])-np.clip(preds[i],0,1)), cmap="gray")
     plt.savefig(mydir+"/bsf_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
     plt.close()
+
--- bsf_test.py
+++ bsf_test.py
@@ -28,7 +28,7 @@ K.set_session(sess)
 import keras
 from keras.datasets import mnist, cifar10
 import matplotlib.pyplot as plt
-from bezier_surface_fitting import bs_fit, bez_filter, bs_build_model, bs_get_weights, bs_set_weights
+from bezier_surface_fitting import bs_fit, bez_filter
 from bezier_surface_plotter import bsplot
 
 img_rows = 32
@@ -48,35 +48,44 @@ n_img = 4
 img_batch = x_train[:n_img,:]
 m = 32
 n = 32
+n_epochs = 100
 
-model = bs_fit(img_batch, m, n)
-K_mat_raw = bs_get_weights(model)
-K_mat = np.reshape(K_mat_raw, (n_img,img_channels,m+1,n+1))
-dummy_input = np.zeros((1, 1)).astype(np.float32)
-pred = model.predict(dummy_input)
+bez_input = bez_filter(img_rows, img_cols, m, n)
+models = []
+for i in range(n_img):
+    models.append(bs_fit(img_batch[i,:], bez_input, m, n, epochs=n_epochs))
+
+
+preds = []
+for i in range(n_img):
+    preds.append(np.reshape(models[i].predict(bez_input), (img_rows,img_cols,img_channels)))
+
 
 # for analysis
 H = 4*img_rows
 W = 4*img_cols
-fine_model = bs_build_model(n_img,img_channels,H,W,m,n)
-bs_set_weights(fine_model, K_mat_raw)
-fine_pred = fine_model.predict(dummy_input)
-mydir = "analysis/chunk_analysis_"+str(m)+"_"+str(n)
+fine_bez_input = bez_filter(H, W, m, n)
+fine_preds = []
+for i in range(n_img):
+    fine_preds.append(np.reshape(models[i].predict(fine_bez_input), (H,W,img_channels)))
+
+
+mydir = "analysis/sep_models_analysis_"+str(m)+"_"+str(n)
 if not os.path.exists(mydir):
     os.makedirs(mydir)
 
 
 for i in range(n_img):
-    bsplot(m, n, H, W, np.transpose(fine_pred[0,i,:],[1,2,0]), mydir+"/bsf_full_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
+    bsplot(m, n, H, W, fine_preds[i], mydir+"/bsf_full_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
     fig=plt.figure(figsize=(8, 8))
     columns = 3
     rows = 1
     fig.add_subplot(rows, columns, 1)
     plt.imshow(np.transpose(img_batch[i,:], [1,2,0]), cmap="gray")
     fig.add_subplot(rows, columns, 2)
-    plt.imshow(np.transpose(np.clip(pred[0, i,:],0,1), [1,2,0]), cmap="gray")
+    plt.imshow(np.clip(preds[i],0,1), cmap="gray")
     fig.add_subplot(rows, columns, 3)
-    plt.imshow(np.transpose(np.abs(img_batch[i,:]-np.clip(pred[0, i,:],0,1)), [1,2,0]), cmap="gray")
+    plt.imshow(np.abs(np.transpose(img_batch[i,:], [1,2,0])-np.clip(preds[i],0,1)), cmap="gray")
     plt.savefig(mydir+"/bsf_" + str(i) + "_" + str(m) + "_" + str(n) + ".png")
     plt.close()
 
